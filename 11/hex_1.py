040dc3335544f2ac76cb96374b26d161dff45bdd
vanderschuea <antoine.vanderschueren@student.uclouvain.be>
2017-12-17 01:56:00 +0100

    day 11

---
+++ 11/hex_1.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+with open("hex.txt") as f:
+    content = f.readlines()
+directions = [x for x in content[0].split(',')]
+
+dic = defaultdict(int)
+x, y = 0, 0
+for d in directions:
+    if d == 'n':
+        y += 1
+    elif d == 's':
+        y -= 1
+    elif d == 'ne':
+        y += 0.5
+        x += 1
+    elif d == 'nw':
+        y += 0.5
+        x -= 1
+    elif d == 'se':
+        y -= 0.5
+        x += 1
+    elif d == 'sw':
+        y -= 0.5
+        x -= 1
+
+print(abs(x) + abs(y) - 0.5*abs(x))  # remove diagonal moves
+
